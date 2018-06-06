cc526ae7bdf56ea3871d664fbebac07f7b1f0209
Oscar Pallas Isern <miorrowls@gmail.com>
2018-06-06 20:55:01 +0200

    Config database platziverse

---
+++ platziverse-db/setup.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const debug = require('debug')('platziverse:db:setup')
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+const db = require('./')
+
+const prompt = inquirer.createPromptModule()
+
+async function setup () {
+  const answer = await prompt([
+    {
+      type: 'confirm',
+      name: 'setup',
+      message: 'This will destroy your database, are you sure?'
+    }
+  ])
+
+  if(!answer.setup){
+    return console.log('Database not destroyed')
+  }
+
+  const config = {
+    database: process.env.DB_NAME || 'platziverse',
+    username: process.env.DB_USER || 'miorrowls',
+    password: process.env.DB_PASS || '112358',
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres',
+    logging: s => debug(s),
+    setup: true,
+    operatorsAliases: false
+  }
+
+  await db(config).catch(handleFatalError)
+
+  console.log('Success!')
+  process.exit(0)
+}
+
+function handleFatalError (err) {
+  console.error(err.message)
+  console.error(err.stack)
+  process.exit(1)
+}
+
+setup()
